44099345770197dc1b4976f47b698ef8f8532dda
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-11-26 00:30:47 +0100

    Add files via upload

---
+++ Players&Monsters/Hero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+
+        public Hero(string username, int level)
+        {
+            this.username = username;
+            this.level = level;
+        }
+
+        public string Username { get { return username; } set { username = value; } }
+        public int Level { get { return level; } set { level = value; } }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
