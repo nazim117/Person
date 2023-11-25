2f17b768ba6883540c37520f8cace5c4b726885e
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-11-26 00:29:14 +0100

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get { return name; } private set { name = value; } }
+        public virtual int Age 
+        {
+            get { return age; } 
+
+            private set 
+            {
+                if (value >= 0)
+                {
+                    age = value; 
+                }
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+            this.Name,
+           this.Age));
+            return stringBuilder.ToString();
+        }
+
+    }
+}
